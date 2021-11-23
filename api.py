8b0d6318e5e2013ed4de777b486540e3a5ed0a1b
rgomeze96 <rgomeze@eafit.edu.co>
2021-11-23 15:10:13 -0500

    trying new server

--- api.py
+++ api.py
@@ -1,11 +1,8 @@
 from flask import Flask, request
-from flask.wrappers import Response
 from flask_cors import CORS, cross_origin
 import json
-import nltk
 import pandas as pd
 import scopus_scrapper as ss
-import numpy as np
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 from string import punctuation
@@ -23,11 +20,15 @@ def nlp_processing():
     else:
         file_from_react = request.files['file']
         df = pd.read_csv(file_from_react)
-        print(df)
         search_params = request.form['searchParams']
         if len(search_params) == 0:
-            return_string = 'Not possible to perfrom NLP without the search parameters'
-            return {'Not Possible': return_string}
+            df_titles = df['Title']
+            number_of_titles = len(df_titles)
+            first_grade_analysis = new_first_grade(df)
+            second_grade_analysis = new_second_grade(df)
+            third_grade_analysis = new_third_grade(df)
+            return {'total_results': number_of_titles, 'results_after_nlp':
+                'NLP not possible without search parameters', 'first_analysis': first_grade_analysis, 'second-analysis': second_grade_analysis, 'third_analysis': third_grade_analysis}
         else:
             tokenized_params = word_tokenize(search_params)
             print('Tokenized parameters: ', tokenized_params)
@@ -42,22 +43,121 @@ def nlp_processing():
             print('First NLP, remove stopwords and punctuation: ', filtered_params)
             df_titles = df['Title']
             df_titles = df_titles.str.lower()
-            print('Document Titles: ', df_titles)
             number_of_titles = len(df_titles)
+            lowered_filtered_params = []
+            for filtered_parameter in filtered_params:
+                filtered_parameter = filtered_parameter.lower()
+                lowered_filtered_params.append(filtered_parameter)
             
-            filtered_results = []
-            for i in range(number_of_titles):
-                print('Title',i, ':', df_titles[i])
-                for filtered_parameter in filtered_params:
-                    filtered_parameter = filtered_parameter.lower()
-                    result_locations = df_titles.str.contains(filtered_parameter)
-                    results = df_titles[result_locations]
+            nlp_filtered_df = df[df['Title'].str.contains('|'.join(lowered_filtered_params), case=False)]
             print('Number of total results: ', number_of_titles)
-            print('Number of filtered results: ', len(results))
-            print('Filtered Results: ')
-            print(results)
-            return {'filtered_params': filtered_params}
+            print('Number of results after NLP: ', len(nlp_filtered_df))
+            first_grade_analysis = new_first_grade(nlp_filtered_df)
+            second_grade_analysis = new_second_grade(nlp_filtered_df)
+            third_grade_analysis = new_third_grade(nlp_filtered_df)
+            print('First Grade Analysis: ', first_grade_analysis)
+            print('Second Grade Analysis: ', second_grade_analysis)
+            print('Third Grade Analysis: ', third_grade_analysis)
 
+            return {'total_results': number_of_titles, 'results_after_nlp': len(nlp_filtered_df), 'first_analysis': first_grade_analysis, 'second-analysis': second_grade_analysis,
+                    'third_analysis': third_grade_analysis}
+
+def new_first_grade(dataframe_for_analysis):
+    df = dataframe_for_analysis
+    # Make object to store publications per year
+    publications_per_year = {}
+    # Get the number of publications per year
+    publications_per_year_pd = df['Year'].value_counts()
+    counter_years = 0
+    # Convert df to Python Object in order to send in return
+    for row in publications_per_year_pd:
+        index_string = str(publications_per_year_pd.index[counter_years])
+        publications_per_year[index_string] = row
+        counter_years += 1
+    
+    publications_per_author = {}
+    authors_df = df['Authors'].str.split(', ')
+    authors_df = authors_df.explode('Authors').reset_index(drop=True)
+    authors_df = authors_df[authors_df != "[No author name available]"]
+    publications_per_author_pd = authors_df.value_counts()
+    counter_authors = 0
+    # Convert df to Python Object in order to send in return
+    for row in publications_per_author_pd:
+        index_string = str(publications_per_author_pd.index[counter_authors])
+        publications_per_author[index_string] = row
+        counter_authors += 1
+    publications_per_affiliations = {}
+    affiliations_df = df['Affiliations'].str.split('; ')
+    affiliations_df = affiliations_df.explode('Affiliations').reset_index(drop=True)
+    publications_per_affiliations_pd = affiliations_df.value_counts()
+    counter_affiliations = 0
+    for row in publications_per_affiliations_pd:
+        index_string = str(publications_per_affiliations_pd.index[counter_affiliations])
+        publications_per_affiliations[index_string] = row
+        counter_affiliations += 1
+    return {'pubs_per_year': publications_per_year, 'pubs_per_author': publications_per_author, 'pubs_per_affiliation': publications_per_affiliations}
+
+def new_second_grade(dataframe_for_analysis):
+    df = dataframe_for_analysis
+    # Separate the authors for each paper
+    authors = df['Authors'].str.split(', ')
+    authors = authors.explode('Authors').reset_index(drop=True)
+    # Remove the fields that have no author
+    authors = authors[authors != "[No author name available]"]
+    # Calculate the number of citations for each author
+    cites_per_author_df = pd.DataFrame(data={author: [df[df['Authors'].str.contains(author, na=False)]['Cited by'].sum()] for author in authors}).T
+    # Create a list to store the authors and the number of publications for each author
+    cites_per_author_list = []
+    # load data into an array in order to send the info to react
+    for author in cites_per_author_df.index:
+        # Make an object in order to store the info into an array
+        author_cite_number = {}
+        author_cite_number[author] = cites_per_author_df.loc[author, 0]
+        cites_per_author_list.append(author_cite_number)
+    
+    # Get all the sources from the dataframe
+    sources = set(df['Source title'])
+    # Calculate the number of citations for each source
+    cites_per_source_df = pd.DataFrame(data={source: [df[df['Source title'].str.contains(source, na=False)]['Cited by'].sum()] for source in sources if type(source) == str}).T
+    # Create a list to store all the info 
+    cites_per_source_list = []
+    for source in cites_per_source_df.index:
+        # Create an object to store in the array of the sources and number of citations
+        source_cite_number = {}
+        source_cite_number[source] = cites_per_source_df.loc[source, 0]
+        cites_per_source_list.append(source_cite_number)
+    
+    # Get all the paper titles from the dataframe
+    papers = set(df['Title'])
+    # Calculate the number of citations for each paper listed
+    cites_per_paper_df = pd.DataFrame(data={paper: [df[df['Title'] == paper]['Cited by'].sum()] for paper in papers}).T
+    # Create an array in order to store the paper titles and number of citations for each paper
+    cites_per_paper_list = []
+    for paper in cites_per_paper_df.index:
+        # Make an object in order to store the paper title and number of publications as key, value to store in array
+        paper_cite_number = {}
+        paper_cite_number[paper] = cites_per_paper_df.loc[paper, 0]
+        cites_per_paper_list.append(paper_cite_number)
+    return {'cites_per_author': cites_per_author_list, 'cites_per_source': cites_per_source_list, 'cites_per_paper': cites_per_paper_list}
+
+def new_third_grade(dataframe_for_analysis):
+    df = dataframe_for_analysis
+    # Separate all the authors
+    authors = df['Authors'].str.split(', ')
+    authors = authors.explode('Authors').reset_index(drop=True)
+    # Remove all the rows with no author
+    authors = authors[authors != "[No author name available]"]
+    # Calculate the number of sources each author is published in
+    sources_per_author = pd.DataFrame(
+    data={author: [df[df['Authors'].str.contains(author, na=False)]['Source title']] for author in authors})
+    num_sources_per_author_df = pd.DataFrame(data={author: [len(sources_per_author[author][0])] for author in authors}).T
+    # Make an array in order to store all the info
+    sources_per_author_list = []
+    for author in num_sources_per_author_df.index:
+        sources_per_author_info = {}
+        sources_per_author_info[author] = int(num_sources_per_author_df.loc[author, 0])
+        sources_per_author_list.append(sources_per_author_info)
+    return {'num_sources_per_author': sources_per_author_list}
 # First grade analysis
 @app.route('/api/first_grade/', methods=['POST'])
 def first_grade():
